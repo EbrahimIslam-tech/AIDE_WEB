3dbb7d8432a7d8a3b41171bce39f9dbeffe21ea0
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-12-28 13:00:52 +0600

    filter folder added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import menu1 from '../photos/menubar.png'
 import menu2 from '../photos/menubar2.png'
 import menu3 from '../photos/menubar3.png'
@@ -13,12 +13,12 @@ import leftIcon from '../photos/Chevron.png'
 
 function Sidebar() {
 
-    const [sideBarExpand,setSidebarExpand] = useState(false)
+    const [sideBarExpand, setSidebarExpand] = useState(false)
 
-    const handleSidebarExpand= ()=>{
-        if(sideBarExpand){
+    const handleSidebarExpand = () => {
+        if (sideBarExpand) {
             setSidebarExpand(false)
-        }else{
+        } else {
             setSidebarExpand(true)
         }
     }
@@ -28,58 +28,58 @@ function Sidebar() {
             <div className="sidebar">
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu1} alt=""/>
+                    <img src={menu1} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu2} alt=""/>
+                    <img src={menu2} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu3} alt=""/>
+                    <img src={menu3} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu4} alt=""/>
+                    <img src={menu4} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu5} alt=""/>
+                    <img src={menu5} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu6} alt=""/>
+                    <img src={menu6} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu7} alt=""/>
+                    <img src={menu7} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu8} alt=""/>
+                    <img src={menu8} alt="" />
                 </div>
 
                 <div className="menuBar_item" onClick={handleSidebarExpand}>
-                    <img src={menu9} alt=""/>
+                    <img src={menu9} alt="" />
                 </div>
 
-                
 
-                
+
+
 
             </div>
 
-            <div className={ sideBarExpand ? "animate__animated animate__slideInLeft animate__faster sidebar_details_container ": "sidebar_details_container containter_hide" }>
+            <div className={sideBarExpand ? "animate__animated animate__slideInLeft animate__faster sidebar_details_container " : "sidebar_details_container containter_hide"}>
                 <div className="sidebar_details_header">
                     <div className="sidebar_header_title">
-                    <img onClick={()=>handleSidebarExpand()} width={9.26} height={16} src={leftIcon} alt=""/>
+                        <img onClick={() => handleSidebarExpand()} width={9.26} height={16} src={leftIcon} alt="" />
                         <p className='title_header'> Category</p>
                     </div>
 
-               </div>
-                    
+                </div>
 
-                    <div class="sidebar_details_body">
+
+                <div class="sidebar_details_body">
 
                     <div className="sidebar_item_selector">
                         <a href="#" className="mysidebar_button selected_button">
@@ -92,46 +92,46 @@ function Sidebar() {
                             Company
                         </a>
                     </div>
-                        <div className="sidebar_searchbar">
-                            <input className='sidebar_input_search' type="text" placeholder='Search' />
-                        </div>
-                        <div className="sidebar_details_content_wrapper">
-                            <ul className="sidebar_list_container">
-                                <li>Food and General Groceries</li>
-                                <li>Health Item</li>
-                                <li>Home Accessories</li>
-                                <li>Beauty Cosmetics</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                                <li>Element 1</li>
-                            </ul>
-                        </div>
+                    <div className="sidebar_searchbar">
+                        <input className='sidebar_input_search' type="text" placeholder='Search' />
                     </div>
-               
+                    <div className="sidebar_details_content_wrapper">
+                        <ul className="sidebar_list_container">
+                            <li>Food and General Groceries</li>
+                            <li>Health Item</li>
+                            <li>Home Accessories</li>
+                            <li>Beauty Cosmetics</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                            <li>Element 1</li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
-            
+
         </div>
     )
 }
